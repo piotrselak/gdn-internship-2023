b73ff729cff1923d5f887ebf38427933375ca072
piotrselak <piotr.selak.it@gmail.com>
2023-04-23 10:35:09 +0200

    Project init

---
+++ gdn-internship-2023/src/main/java/com/github/piotrselak/gdninternship2023/GdnInternship2023Application.java
@@ -0,0 +1,13 @@
+package com.github.piotrselak.gdninternship2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdnInternship2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdnInternship2023Application.class, args);
+	}
+
+}
